367bc63a89c79ce8e2785e9de5d771caebb2cb25
callotravellk-ship-it <callotravel.lk@gmail.com>
2026-09-08 03:47:15 +0000

    updated both the Google Tags and Meta Pixel inline scripts to use

--- tours-coach-next/src/app/layout.jsx
+++ tours-coach-next/src/app/layout.jsx
@@ -97,41 +97,49 @@ export default function RootLayout({ children }) {
            src="https://www.googletagmanager.com/gtag/js?id=G-42VZGH60KM" 
            strategy="afterInteractive" 
          />
-         <Script id="google-tags-init" strategy="afterInteractive">
-           {`
-             window.dataLayer = window.dataLayer || [];
-             function gtag(){dataLayer.push(arguments);}
-             gtag('js', new Date());
-             
-             // Initialize Google Analytics
-             gtag('config', 'G-42VZGH60KM');
-             
-             // Initialize Google Ads
-             gtag('config', 'AW-18421379566');
+         <Script 
+           id="google-tags-init" 
+           strategy="afterInteractive"
+           dangerouslySetInnerHTML={{
+             __html: `
+               window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+               
+               // Initialize Google Analytics
+               gtag('config', 'G-42VZGH60KM');
+               
+               // Initialize Google Ads
+               gtag('config', 'AW-18421379566');
 
-             // Google Ads Phone Conversion Tracking
-             gtag('config', 'AW-18421379566/hkc2COWC-fAcEO7j_89E', {
-               'phone_conversion_number': '(416) 269-9555'
-             });
-           `}
-         </Script>
+               // Google Ads Phone Conversion Tracking
+               gtag('config', 'AW-18421379566/hkc2COWC-fAcEO7j_89E', {
+                 'phone_conversion_number': '(416) 269-9555'
+               });
+             `
+           }}
+         />
 
          {/* 2. META PIXEL SCRIPT BLOCK */}
-         <Script id="meta-pixel" strategy="afterInteractive">
-           {`
-             !function(f,b,e,v,n,t,s)
-             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-             n.queue=[];t=b.createElement(e);t.async=!0;
-             t.src=v;s=b.getElementsByTagName(e)[0];
-             s.parentNode.insertBefore(t,s)}(window, document,'script',
-             'https://connect.facebook.net/en_US/fbevents.js');
-             
-             fbq('init', '968344975840291');
-             fbq('track', 'PageView');
-           `}
-         </Script>
+         <Script 
+           id="meta-pixel" 
+           strategy="afterInteractive"
+           dangerouslySetInnerHTML={{
+             __html: `
+               !function(f,b,e,v,n,t,s)
+               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+               n.queue=[];t=b.createElement(e);t.async=!0;
+               t.src=v;s=b.getElementsByTagName(e)[0];
+               s.parentNode.insertBefore(t,s)}(window, document,'script',
+               'https://connect.facebook.net/en_US/fbevents.js');
+               
+               fbq('init', '968344975840291');
+               fbq('track', 'PageView');
+             `
+           }}
+         />
       </head>
       <body>
         

1249770ba9e129cd7512b58181d452f2f2f624fb
callotravellk-ship-it <callotravel.lk@gmail.com>
2026-09-09 04:43:14 +0000

    dddwr

--- tours-coach-next/src/app/layout.jsx
+++ tours-coach-next/src/app/layout.jsx
@@ -141,10 +141,25 @@ export default function RootLayout({ children }) {
              `
            }}
          />
+
+         {/* 3. MICROSOFT CLARITY SCRIPT BLOCK */}
+         <Script 
+           id="microsoft-clarity" 
+           strategy="afterInteractive"
+           dangerouslySetInnerHTML={{
+             __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "msfluq1gc6");
+             `
+           }}
+         />
       </head>
       <body>
         
-        {/* 3. META PIXEL NOSCRIPT FALLBACK */}
+        {/* 4. META PIXEL NOSCRIPT FALLBACK */}
         <noscript>
           <img 
             height="1" 
